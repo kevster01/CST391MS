7f61f84e47e29c40ca2d9a3b3348a3bbf7c9f6ab
kevster01 <90598902+kevster01@users.noreply.github.com>
2023-01-09 19:31:13 -0800

    Add files via upload

---
+++ src/app/admin/dashboard/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.scss']
+})
+export class PostCreateComponent implements OnInit{
+  constructor(public postService: PostsService){ }
+  ngOnInit(): void {
+      
+  }
+  onPostAdded(form: NgForm){
+    if(form.invalid)
+    {
+      return 
+    }
+    this.postService.addPost(form.value.title, form.value.content, form.value.dateTime);
+    form.resetForm()
+
+  }
+
+}
